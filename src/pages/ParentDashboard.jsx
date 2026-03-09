750732b758ca8da344bb38a4c07e4870abf5a1e8
Şahin Başdoğan <06thyk3@gmail.com>
2026-03-09 08:49:02 +0000

    File changes

--- src/pages/ParentDashboard.jsx
+++ src/pages/ParentDashboard.jsx
@@ -166,8 +166,7 @@ export default function ParentDashboard() {
       {/* Full Payment History */}
       <PaymentHistory payments={payments} />
 
-      {/* Teacher Chat */}
-      <ParentChat student={student} user={user} />
+
     </div>
   );
 }
